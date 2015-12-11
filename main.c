887851fb6c80e7749570dea4b2e137b7a16266b5
Arnie97 <arnie97@gmail.com>
2015-12-11 16:41:42 +0800

    Support all fonts in HZK series.

--- main.c
+++ main.c
@@ -3,10 +3,8 @@
 #include <string.h>
 
 void *
-get_bitmap_font(void *buf, const unsigned char *bytes, size_t offset)
+get_bitmap_font(void *buf, const unsigned char *bytes, size_t offset, size_t size)
 {
-    size_t offset_size = 16 * 16 / 8;
-
     // 先求区位码
     size_t code_point =
         (bytes[0] - 0xA1)    // 区码
@@ -16,14 +14,14 @@ get_bitmap_font(void *buf, const unsigned char *bytes, size_t offset)
 
     // 然后再计算其在区位码二维表中的位置，进而得出此字符在文件中的偏移
     if (bytes[0] >= 0xA1) {
-        offset += code_point * offset_size;
+        offset += code_point * size;
     } else {
-        offset += (bytes[0] + 156 - 1) * offset_size;
+        offset += (bytes[0] + 156 - 1) * size;
     }
 
     FILE *fp = fopen("res/font/HZK16", "rb");
     fseek(fp, offset, SEEK_SET);
-    fread(buf, 1, offset_size, fp);
+    fread(buf, 1, size, fp);
     fclose(fp);
     fp = NULL;
     return buf;
@@ -34,22 +32,29 @@ main(int argc, const char *argv[])
 {
     unsigned char *str = "\xc4\xfe\xbe\xb2\xd6\xc2\xd4\xb6";
     size_t len = strlen(str);
-
     for (size_t i = 0; i < len; ++i) {
         printf("0x%x ", str[i]);
     }
     putchar('\n');
 
-    uint16_t font_data[16];
+    size_t rows = 16, cols = 16;
+    size_t offset_size = rows * cols / 8;
+    uint8_t font_data[offset_size];
+
     for (size_t i = 0; i < len; i += 2) {
-        get_bitmap_font(font_data, str + i, 0);
+        get_bitmap_font(font_data, str + i, 0, offset_size);
         putchar('\n');
-        for (int row = 0; row < 16; row++) {
-            for (int col = 7; col >= 0; col--) {
-                fputs(((font_data[row] >> col) & 1)? "##": "  ", stdout);
-            }
-            for (int col = 15; col >= 8; col--) {
-                fputs(((font_data[row] >> col) & 1)? "@@": "  ", stdout);
+
+        uint8_t *ptr = font_data, pos = 7;
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                fputs(((*ptr >> pos) & 1)? "@@": "  ", stdout);
+                if (pos) {
+                    pos--;
+                } else {
+                    pos = 7;
+                    ptr++;
+                }
             }
             putchar('\n');
         }
