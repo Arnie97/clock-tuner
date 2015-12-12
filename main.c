c7b55bae3d4aa0bea3a9c0ae74e907094229e0bd
Arnie97 <arnie97@gmail.com>
2015-12-13 00:01:45 +0800

    Integrate the bitmap module into main program.

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 
 void *
@@ -24,22 +25,39 @@ get_bitmap_font(void *buf, const unsigned char *bytes[], size_t offset, size_t s
     return buf;
 }
 
-void
-bitmap_blit(const unsigned char *str)
+uint8_t *
+bitmap_blit(uint32_t width, uint32_t height, const unsigned char *str)
 {
     const size_t
     rows          = 16,
     cols          = 16,
     offset_size   = rows * cols / 8;
 
+    const uint16_t
+    bpp           = 1;
+
+    const uint32_t
+    bytes_per_row = (width * bpp + 31) / 32 * 4,
+    data_size     = bytes_per_row * height;
+
+    uint8_t *buf = malloc(data_size);
+    if (!buf) {
+        printf("Memory allocation FAILED!\n");
+        return NULL;
+    } else {
+        memset(buf, 0, data_size);
+    }
+
     uint8_t font_data[offset_size];
+    size_t x = 0, y = height;
     for (const unsigned char *beg = str, *end = str + strlen(str); beg != end;) {
         get_bitmap_font(font_data, &beg, 0, offset_size);
         putchar('\n');
         uint8_t *ptr = font_data, pos = 7;
-        for (size_t row = 0; row < rows; row++) {
-            for (size_t col = 0; col < cols; col++) {
-                fputs(((*ptr >> pos) & 1)? "@@": "  ", stdout);
+        for (size_t row = 0; row < rows; row++, y--) {
+            for (size_t col = 0; col < cols; col++, x++) {
+                printf("buf[%u,%u] = font[%u,%u] = %u\n", y, x, row, col, (*ptr >> pos) & 1);
+                buf[y * bytes_per_row + (x >> 3)] |= ((*ptr >> pos) & 1) << (7 - x & 7);
                 if (pos) {
                     pos--;
                 } else {
@@ -47,9 +65,18 @@ bitmap_blit(const unsigned char *str)
                     ptr++;
                 }
             }
+            x -= cols;
             putchar('\n');
         }
+        if (x < 132) {
+            x += cols;
+            y += rows;
+        } else {
+            x = 0;
+            y++;
+        }
     }
+    return buf;
 }
 
 int
@@ -58,13 +85,17 @@ main(int argc, const char *argv[])
     // parse_args();
     // read_file();
 
+    const uint32_t
+    width         = 131,
+    height        = 64;
+
     const unsigned char
     text[]        = "#!\xc4\xfe\xbe\xb2\xd6\xc2\xd4\xb6",
     output_file[] = "__ADK__.BMP";
 
-    bitmap_blit(text);
-    // write_1_bit_bmp(output_file, width, height, buf);
-    // free(buf);
+    uint8_t *buf = bitmap_blit(width, height, text);
+    write_1_bit_bmp(output_file, width, height, buf);
+    free(buf);
 
     return 0;
 }
