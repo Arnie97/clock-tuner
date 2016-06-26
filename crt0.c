26589783ee1ae98d3cceee1c63d611fce1fbb755
Arnie97 <arnie97@gmail.com>
2016-06-26 21:34:52 +0800

    Import skeleton of crt0.

---
+++ crt0.c
@@ -0,0 +1,73 @@
+/* Derived from /hpgcc/sources/crt0/crt0.c of HP-GCC Version 1.1
+
+Copyright (C) 2005 The HP-GCC Development Team
+Copyright (C) 2016 Arnie97
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*/
+
+#include <hpgcc49.h>
+
+extern unsigned int *__exit_stk_state;
+extern int _exit_save(unsigned int *ptr);
+extern void __exit_cleanup();
+
+// default monochrome display buffer
+unsigned __display_buf;
+
+
+void _start()
+{
+	// read in the video buffer address
+	// it assumes that the MMU mappings haven't changed
+	// and the buffer is in the 4kb SRAM
+	volatile unsigned *LCDSADDR1 = (unsigned *)0x7300014;
+	__display_buf = (*LCDSADDR1 & 0xffff) * 2 + 0x7f00000;
+
+	// set the screen height for the <hpstdio.h> module
+	extern int __scr_h;
+	__scr_h = (sys_lcdgetheight() == 64)? 10: 13;
+
+	// declare locally to reduce executable size
+	unsigned state_buffer[4], lcd_buffer[17], clk_buffer[6];
+	__exit_stk_state = state_buffer;
+
+	// turn interrupts off so we can use the screen
+	sys_intOff();
+
+	// save current hardware state
+	sys_lcdsave(lcd_buffer);
+	sys_clksave(clk_buffer);
+
+	// slow mode on by default - makes sense in the majority of applications
+	sys_slowOn();
+
+	// will return 0 when exitting
+	if (_exit_save((unsigned *)state_buffer)) {
+		clear_screen();
+		printf("Hello World!\n\n");
+		printf("PRESS [APLET] KEY TO EXIT");
+		while (!keyb_isAnyKeyPressed());
+		__exit_cleanup();
+	}
+
+	// restore original hardware values
+	sys_lcdrestore(lcd_buffer);
+	sys_clkrestore(clk_buffer);
+
+	// turn interrupts on for the OS
+	sys_intOn();
+}
