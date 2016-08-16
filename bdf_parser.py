c91b0bf9de65a54518650dd53aefe966a9df357f
Arnie97 <arnie97@gmail.com>
2016-08-16 16:18:34 +0800

    Parse width parameter in BBX statements.

--- bdf_parser.py
+++ bdf_parser.py
@@ -2,6 +2,16 @@
 
 import sys
 
+
+def msb(n):
+    'Return position of the most significant bit in an integer.'
+    i = 0
+    while n:
+        i += 1
+        n >>= 1
+    return i
+
+
 for line in sys.stdin:
     group = line.split()
 
@@ -24,12 +34,25 @@ for line in sys.stdin:
         width, height, x0, y0 = map(int, group[1:5])
         print(width, height, x0, y0)
     elif group[0] == 'BITMAP':
+        buffer, offset = [], 0
         while True:
             line = sys.stdin.readline().strip()
-            if line == 'ENDCHAR':
-                break
+            if line != 'ENDCHAR':
+                hex_data = int(line, 16)
+                buffer.append(hex_data)
+                offset = max(offset, msb(hex_data))
 
-            hex_data = int(line, 16)
-            print(format(hex_data, '016b').translate({
-                ord('0'): '..', ord('1'): '##'
-            }))
+            else:
+                offset -= width
+                print('\n'.join(
+                    format(
+                        hex_data >> offset
+                        if offset > 0 else
+                        hex_data << -offset,
+                        '0%db' % width
+                    ).translate({
+                        ord('0'): '..', ord('1'): '##'
+                    })
+                    for hex_data in buffer
+                ), '\n')
+                break
